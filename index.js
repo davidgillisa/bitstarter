6fb7aa9fe541b211300a14f6cf154e9195b13139
David Gillis <davidgillisa@gmail.com>
2015-06-13 01:45:08 +0000

    committed

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
+
+var mybuf;
+app.use(express.logger());
+
+ mybuf=fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var buffer=new Buffer(256);
-  response.send(buffer.toString('utf-8',fs.readFileSync("index.html")));
-}
-);
+  response.send(mybuf.toString());
+});
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {console.log("Listening on " + port);
-}
-);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
