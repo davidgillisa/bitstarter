bddda350ab456e16049efbb80ce320760d4c397d
David Gillis <davidgillisa@gmail.com>
2015-06-13 00:59:25 +0000

    added index.html capabilities

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/index.html', function(request, response) {
+  var fs = require('fs');
+  var buffer=new Buffer(256);
+  response.send(buffer.toString('utf-8',fs.readFileSync("index.html")));
 })
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
